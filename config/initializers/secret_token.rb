545cdf81a3c45e99a97a54ce75170788809598bc
burnssa@gmail.com <burnssa@gmail.com>
2014-08-31 19:22:44 -0700

    Initial templating

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookmarkme::Application.config.secret_key_base = '51d175146215d60d52c4e9df2fbf02af684ed58fc2fbbf7a2ddcfefe725d7b9c05952db75631a70b033ee1b737bbd66c68494319d18f085475868470bbbfcc63'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bookmarkme::Application.config.session_store :cookie_store, key: '_bookmarkme_session'
