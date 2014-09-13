fba66dba78f0c4ebee86cf8537fe556860ceff7c
burnssa@gmail.com <burnssa@gmail.com>
2014-09-12 21:54:23 -0700

    added mailgun route

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -8,6 +8,27 @@ class IncomingController < ApplicationController
     # to get a sense of what you're dealing with.
     puts "INCOMING PARAMS HERE: #{params}"
 
+    # find the user by the sender 
+    current_user = User.find_by_email(params[:sender])
+
+    bookmark = Bookmark.new(url: params['stripped-text'])
+
+    bookmark.user = current_user
+
+    topic_names = params[:subject].split(' ')
+    topic_descriptions = []
+    topic_names.each {|topic| topic_descriptions << topic.sub(/#/, '')}
+
+    topic_descriptions.each do |t|
+      if current_user.topics.find { |topic| topic[:description] == t} == true
+        bookmark.topic = t
+      else
+        bookmark.topic = Topic.new(description: t)
+      end     
+    end
+
+    bookmark.save
+
     # You put the message-splitting and business
     # magic here. 
 
