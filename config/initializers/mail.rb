b836184680702163080b91e4d3c2079698211116
burnssa@gmail.com <burnssa@gmail.com>
2014-09-13 15:08:12 -0700

    updated password

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -14,7 +14,7 @@ ActionMailer::Base.raise_delivery_errors = true
 
 class DevelopmentMailInterceptor
   def self.delivering_email(message)
-    message.to =  'postmaster@bookmarkme.co'
+    message.to = 'postmaster@bookmarkme.co'
     message.cc = nil
     message.bcc = nil
   end
