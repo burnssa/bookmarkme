e80d656b3eafb1de23c7404430efff63fd13b8c6
burnssa@gmail.com <burnssa@gmail.com>
2014-09-16 23:14:12 -0700

    set mock user if sender not current v8

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,7 +2,7 @@ class Topic < ActiveRecord::Base
   has_many :bookmarks
   belongs_to :user
 
-	def current_topic_titles
+	def self.get_current_topic_titles
 		current_topic_titles = Topic.all.collect { |t| t.title }
 	end
 end
