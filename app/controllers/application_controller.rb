7d8de8b89f52934e5579e45f2e837244e8bd5e5f
burnssa@gmail.com <burnssa@gmail.com>
2014-09-27 16:02:49 -0700

    added application controller logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+	before_filter :show_topics_page
 
 private
 	def current_user
@@ -12,5 +13,9 @@ private
 	def authorize
 		redirect_to topics_path, alert: "Not authorized"
 	end
+
+  def show_topics_page
+    render topics_path if current_user?  # you have to define maintenance?
+  end
 
 end
