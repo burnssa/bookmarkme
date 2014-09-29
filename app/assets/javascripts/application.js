3a4bdda85034f51b8353cf1d960652dbdf1543d7
burnssa@gmail.com <burnssa@gmail.com>
2014-09-28 19:51:26 -0700

    re-written snippet

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,16 +23,16 @@ var blocmetrics = (function () {
     var submitted = {};
     _bm_event = {};
 
-    submitted.track = function(name, arg_1, arg_2, arg_3) {
+    submitted.track = function(name, arg_1, arg_2) {
             _bm_event.name = name;
-            _bm_event.property_1 = arg_1.property_1;
-            _bm_event.property_2 = arg_2.property_2;
+            _bm_event.property_1 = arg_1;
+            _bm_event.property_2 = arg_2;
             _send_data();
     };
 
     function _send_data() {
         _bm_request = new XMLHttpRequest();
-        _bm_request.open("POST", "http://localhost:3000/events.json", true);
+        _bm_request.open("POST", "http://lvh.me:3000/events.json", true);
         _bm_request.setRequestHeader('Content-Type', 'application/json');
         _bm_request.onreadystatechange = function () {
 
